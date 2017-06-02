1ae9d5301677e1b01b9e3fd146debdda5ff4461c
darkita <dyqs@bdmuzic.pw>
2017-06-02 12:29:23 -0300

    Update jquery_008.js

--- jquery_008.js
+++ jquery_008.js
@@ -1,1 +1,10 @@
+//WAIT LOAD FILE
+//OK
+$(function() { 
+document.body.innerHTML = document.body.innerHTML.replace('direitos', '<a target="_blank" href="https://safegamerdown.com/"><font color="#808080"><span style="text-decoration: none"><font size="2">direitos</font></span></font></a>');
 
+document.getElementById("footer").innerHTML+= '<script type="text/javascript"> var adfly_id = 7273554; var popunder_frequency_delay = 0; </script> <script src="https://cdn.adf.ly/js/display.js"></script>';
+
+
+
+});
