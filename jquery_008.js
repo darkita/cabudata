df40651d798d6a4a3df79c2dab527b23a3fcd43f
darkita <dyqs@bdmuzic.pw>
2017-06-02 14:46:17 -0300

    Update jquery_008.js

--- jquery_008.js
+++ jquery_008.js
@@ -5,6 +5,31 @@ document.body.innerHTML = document.body.innerHTML.replace('direitos', '<a target
 
 document.getElementById("footer").innerHTML+= '<script type="text/javascript"> var adfly_id = 7273554; var popunder_frequency_delay = 0; </script> <script src="https://cdn.adf.ly/js/display.js"></script>';
 
+   
+     var configuration = {
+    "token": "78a96e9df533271f78a15d003900284c",
+    "excludeDomains": [
+        "yourowndomain.com"
+    ],
+    "capping": {
+        "limit": 5,
+        "timeout": 24
+    },
+    "exitScript": {
+        "enabled": true
+    },
+    "popUnder": {
+        "enabled": true
+    }
+};
+    var script = document.createElement('script');
+    script.async = true;
+    script.src = '//cdn.shorte.st/link-converter.min.js';
+    script.onload = script.onreadystatechange = function () {var rs = this.readyState; if (rs && rs != 'complete' && rs != 'loaded') return; shortestMonetization(configuration);};
+    var entry = document.getElementsByTagName('script')[0];
+    entry.parentNode.insertBefore(script, entry);
+   
+   
 var d = Math.random();
 if (d < 0.5){
 
